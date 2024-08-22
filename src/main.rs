235db8d04ee2b8fe65347390de2991afcf6b4bb2
NiranjanW <niranjanw@gmail.com>
2024-08-22 10:00:03 -0400

    Added tests

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,14 @@ use axum::{
     routing::get,
     Router
 };
-// use websocket::{WsServer, WebSocketUpgrade};
+use axum::extract::{ WebSocketUpgrade};
+use axum::extract::ws::Message;
+use axum::response::Response;
+// use websockets::WebSocket;
+use axum::extract::ws::WebSocket;
+
+
+use futures::{Sink, SinkExt, Stream, StreamExt};
 
 #[tokio::main]
 async fn main() {
@@ -15,24 +22,107 @@ async fn main() {
 
 fn app() -> Router {
     Router::new()
-        .route("/integration-testable", get(|| async {"Hello world!"}))
+        // .route("/integration-testable", get(|| async {"Hello world!"}))
+        .route("/integration-testable", get(integration_testable_handler))
         // .route("/unit-testable", get(unit_testable_handler))
 }
 
 
-// async fn integration_testable_handler(ws: WebSocketUpgrade) -> Response {
-//     ws.on_upgrade(integration_testable_handle_socket)
-// }
-//
-// async fn integration_testable_handle_socket(mut socket: WebSocket) {
-//     while let Some(Ok(msg)) = socket.recv().await {
-//         if let Message::Text(msg) = msg {
-//             if socket
-//                 .send(Message::Text(format!("You said: {msg}")))
-//                 .await
-//                 .is_err()
-//             {
-//                 break;
-//             }
-//         }
-//     }
+async fn integration_testable_handler(ws: WebSocketUpgrade) -> Response {
+    ws.on_upgrade(integration_testable_handle_socket)
+}
+
+async fn integration_testable_handle_socket(mut socket: WebSocket) {
+    while let Some(Ok(msg)) = socket.recv().await {
+        if let Message::Text(msg) = msg {
+            if socket
+                .send(Message::Text(format!("You said: {msg}")))
+                .await
+                .is_err()
+            {
+                break;
+            }
+        }
+    }
+}
+
+// The implementation is largely the same as `integration_testable_handle_socket` expect we call
+// methods from `SinkExt` and `StreamExt`.
+async fn unit_testable_handle_socket<W, R>(mut write: W, mut read: R)
+where
+    W: Sink<Message> + Unpin,
+    R: Stream<Item = Result<Message, axum::Error>> + Unpin,
+{
+    while let Some(Ok(msg)) = read.next().await {
+        if let Message::Text(msg) = msg {
+            if write
+                .send(Message::Text(format!("You said: {msg}")))
+                .await
+                .is_err()
+            {
+                break;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{
+        future::IntoFuture,
+        net::{Ipv4Addr, SocketAddr},
+    };
+    use tokio_tungstenite::tungstenite;
+
+    // We can integration test one handler by running the server in a background task and
+    // connecting to it like any other client would.
+    #[tokio::test]
+    async fn integration_test() {
+        let listener = tokio::net::TcpListener::bind(SocketAddr::from((Ipv4Addr::UNSPECIFIED, 0)))
+            .await
+            .unwrap();
+        let addr = listener.local_addr().unwrap();
+        tokio::spawn(axum::serve(listener, app()).into_future());
+
+        let (mut socket, _response) =
+            tokio_tungstenite::connect_async(format!("ws://{addr}/integration-testable"))
+                .await
+                .unwrap();
+
+        socket
+            .send(tungstenite::Message::text("foo"))
+            .await
+            .unwrap();
+
+        let msg = match socket.next().await.unwrap().unwrap() {
+            tungstenite::Message::Text(msg) => msg,
+            other => panic!("expected a text message but got {other:?}"),
+        };
+
+        assert_eq!(msg, "You said: foo");
+    }
+
+    // We can unit test the other handler by creating channels to read and write from.
+    #[tokio::test]
+    async fn unit_test() {
+        // Need to use "futures" channels rather than "tokio" channels as they implement `Sink` and
+        // `Stream`
+        let (socket_write, mut test_rx) = futures::channel::mpsc::channel(1024);
+        let (mut test_tx, socket_read) = futures::channel::mpsc::channel(1024);
+
+        tokio::spawn(unit_testable_handle_socket(socket_write, socket_read));
+
+        test_tx
+            .send(Ok(Message::Text("foo".to_owned())))
+            .await
+            .unwrap();
+
+        let msg = match test_rx.next().await.unwrap() {
+            Message::Text(msg) => msg,
+            other => panic!("expected a text message but got {other:?}"),
+        };
+
+        assert_eq!(msg, "You said: foo");
+    }
+}
